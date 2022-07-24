59ae6572e983dda2089e3bd1ab7bf76da1194e11
Ayoub Imad <Imad190800@gmail.com>
2022-07-24 18:09:33 +0200

    oke

---
+++ src/main/java/Utils/Book.java
@@ -0,0 +1,105 @@
+package Utils;
+
+import java.sql.Blob;
+import java.util.Objects;
+
+public class Book {
+    String isbn;
+    String titolo;
+    String autore;
+    String università;
+    Blob immagine;
+    int prezzo;
+    String descrizione;
+    int quantità;
+
+    public Book(String isbn, String titolo, String autore, String università, Blob immagine, int prezzo, String descrizione, int quantità) {
+        this.isbn = isbn;
+        this.titolo = titolo;
+        this.autore = autore;
+        this.università = università;
+        this.immagine = immagine;
+        this.prezzo = prezzo;
+        this.descrizione = descrizione;
+        this.quantità = quantità;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Book book = (Book) o;
+        return Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getUniversità() {
+        return università;
+    }
+
+    public void setUniversità(String università) {
+        this.università = università;
+    }
+
+    public Blob getImmagine() {
+        return immagine;
+    }
+
+    public void setImmagine(Blob immagine) {
+        this.immagine = immagine;
+    }
+
+    public int getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(int prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public int getQuantità() {
+        return quantità;
+    }
+
+    public void setQuantità(int quantità) {
+        this.quantità = quantità;
+    }
+}
